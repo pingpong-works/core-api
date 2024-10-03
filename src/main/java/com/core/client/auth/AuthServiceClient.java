e0fe9000ab970dd8b6fca6d6a9caf8541dbde7ab
Hyeji <165968530+quokkavely@users.noreply.github.com>
2024-10-03 19:42:15 +0900

    feat Add Member of EDMS(#22), edit url

--- src/main/java/com/core/client/auth/AuthServiceClient.java
+++ src/main/java/com/core/client/auth/AuthServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "auth-api", url = "http://localhost:54390")
+@FeignClient(name = "auth-api")
 public interface AuthServiceClient {
 
     @GetMapping("/employees/{id}")
